9c81b73b675f7a50cecff948446d2a93caa7dac1
syuhas <syuhas22@gmail.com>
2024-12-07 18:08:40 -0500

    updated gitignore

--- create_database.py
+++ create_database.py
@@ -5,7 +5,9 @@ from sqlalchemy.orm import sessionmaker
 from goalt_app.models import Base, User, Goal
 
 
-engine = create_engine('sqlite:///test.db')
-
+engine = create_engine('sqlite:///database.db')
+#delete database if it already exists
+Base.metadata.drop_all(engine)
 # grab those models and create them in the local db file
 Base.metadata.create_all(engine)
+
