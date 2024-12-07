b13f0a40a97f3b761c692c124250c52d27f5e726
syuhas <syuhas22@gmail.com>
2024-12-07 18:35:16 -0500

    db name

--- goalt_app/__init__.py
+++ goalt_app/__init__.py
@@ -9,7 +9,7 @@ def create_app():
     app = Flask(__name__)
     # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://master:Sucesso_24@localhost/goal_tracker'
     app.secret_key = 'b20a61da639d856a1ba0079df2b26c87'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
