74e0af16c1ff3aca32caa6e797985c84fa9b1e91
KaPozzi <kapozzi@gmail.com>
2025-02-22 00:38:27 -0300

    parse json

--- goalt_app/secrets.py
+++ goalt_app/secrets.py
@@ -1,15 +1,24 @@
-import boto3
+import boto3, json
 
 ## Get RDS secret from secrets manager ##
 
-client = boto3.client('secretsmanager')
+client = boto3.client('secretsmanager', region_name='us-east-1')
 
 def get_secret():
     response = client.get_secret_value(
-        SecretId = 'arn:aws:secretsmanager:us-east-1:061039789243:secret:rds!db-555390f8-60f2-4d37-ad75-e63d8f0cbfa9-0s9oyX',
-        region = 'us-east-1'
+        SecretId = 'arn:aws:secretsmanager:us-east-1:061039789243:secret:rds!db-555390f8-60f2-4d37-ad75-e63d8f0cbfa9-0s9oyX'
     )
-    return response
 
+    try:
+        secret_response = client.get_secret_value(SecretId='arn:aws:secretsmanager:us-east-1:061039789243:secret:rds!db-555390f8-60f2-4d37-ad75-e63d8f0cbfa9-0s9oyX')
+        secret = secret_response['SecretString']
+        json_secret = json.loads(secret)
+        credentials = {
+            'username': json_secret['username'],
+            'password': json_secret['password']
+        }
+        return credentials
+    except ClientError as e:
+        raise e
+    
 print(get_secret())
-
